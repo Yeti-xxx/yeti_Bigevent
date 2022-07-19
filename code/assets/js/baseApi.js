7470782fb529a3b7539820f6a1f650d888c60ad7
Yetixxx <2272970495@qq.com>
2022-07-19 11:00:40 +0800

    完成主頁功能開發

--- code/assets/js/baseApi.js
+++ code/assets/js/baseApi.js
@@ -1,4 +1,20 @@
 // 统一拼接路劲
 $.ajaxPrefilter(function (options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    //统一为有权限接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token'),
+        };
+    }
+    //全局统一挂载complete
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1) {
+            //清空本地token
+            localStorage.removeItem('token');
+            //跳转登录页面
+            location.href = '/Node.js/0大事件前端/code/login.html'
+        }
+    }
 })
+
---
+++ code/assets/js/index.js
@@ -0,0 +1,46 @@
+$(function () {
+    getUserInfo();
+
+})
+//获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            else {
+                renderAvatar(res.data);
+            }
+        },
+        
+
+    })
+
+}
+//渲染头像函数
+function renderAvatar(user) {
+    const name = user.nickname || user.username;
+    $('#welcome').html('欢迎 ' + name);
+    //按需渲染用户头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('text-avater').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        //渲染文本头像
+        const firstword = name[0].toUpperCase();
+        $('.text-avater').html(firstword).show();
+    }
+}
+
+//退出功能
+$('#btnlogout').on('click', function () {
+    //清空本地token
+    localStorage.removeItem('token');
+    //跳转登录页面
+    location.href = '/Node.js/0大事件前端/code/login.html'
+
+})
